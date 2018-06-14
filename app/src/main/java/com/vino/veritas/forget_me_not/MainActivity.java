227e4d1a9798fbf0fa16aed63a5fd49832644e25
Aniket Kaulavkar <aniket.kaulavkar@gmail.com>
2018-06-14 10:18:07 +0530

    Initial Commit

---
+++ app/src/main/java/com/vino/veritas/forget_me_not/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vino.veritas.forget_me_not;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
